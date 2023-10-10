f3d2ebec9a9ef7cf0b7853f9d490bec1ef123e91
Victoria Saravia <victoria.saravia@idatha.com>
2023-10-10 13:58:09 -0300

    functions, variables

---
+++ indoor/faces.py
@@ -0,0 +1,24 @@
+#implement a function called convert that accepts a str 
+#as input and returns that same input with any :) converted 
+#to 🙂 (otherwise known as a slightly smiling face) and 
+#any :( converted to 🙁 (otherwise known as a slightly 
+#frowning face). All other text should be returned unchanged.
+
+#Then, in that same file, implement a function called main 
+#that prompts the user for input, calls convert on that input, 
+#and prints the result. You’re welcome, but not required, to 
+#prompt the user explicitly, as by passing a str of your own 
+#as an argument to input. Be sure to call main at the bottom 
+#of your file.
+
+def convert(input_str):
+    input_str = input_str.replace(":)", "🙂")
+    input_str = input_str.replace(":(", "🙁")
+    return input_str
+
+def main():
+    user_input = input("Enter a text: ")
+    print("Text:", convert(user_input))
+
+if __name__ == "__main__":
+    main()
---
+++ indoor/indoor.py
@@ -0,0 +1,9 @@
+#In a file called indoor.py, implement a program in Python that prompts 
+#the user for input and then outputs that same input in lowercase. 
+#Punctuation and whitespace should be outputted unchanged. You’re welcome, 
+#but not required, to prompt the user explicitly, as by passing a str of 
+#your own as an argument to input.
+
+input_str = (input("Write something: ")).lower()
+
+print(f"Lower input: {input_str}")
