18a32eb98a6212bc84a3b3a1f9f0e7f75d7d0711
Victoria Saravia <victoria.saravia@idatha.com>
2023-10-12 07:58:24 -0300

    loops - camel

--- set2/camel.py
+++ set2/camel.py
@@ -4,3 +4,22 @@ camel case and outputs the corresponding name in snake case. Assume that
 the user's input will indeed be in camel case.
 """
 
+def main():
+    camelCase = input("camelCase: ")
+
+    print(f"snake_case: {convert(camelCase)}")
+
+
+def convert(phrase):
+
+    snake_case = phrase.lower()
+
+    for i, char in enumerate(phrase):
+        if char.isupper():
+            snake_case = snake_case[0:i] + "_" + snake_case[i:]
+
+    return snake_case
+
+
+if __name__ == "__main__":
+    main()
