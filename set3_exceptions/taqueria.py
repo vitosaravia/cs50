a737e8d9930ec8bbb4669c26646af36ba2d62d87
Victoria Saravia <victoria.saravia@idatha.com>
2023-10-17 09:42:29 -0300

    exceptions - taqueria

--- set3_exceptions/taqueria.py
+++ set3_exceptions/taqueria.py
@@ -12,8 +12,9 @@ key is a price in dollars:
     "Super Burrito": 8.50,
     "Super Quesadilla": 9.50,
     "Taco": 3.00,
-    "Tortilla Salad": 8.00
+    "Tortilla Salad": 8.00,
 }
+
 In a file called taqueria.py, implement a program that enables a user to place 
 an order, prompting them for items, one per line, until the user inputs control-d 
 (which is a common way of ending one's input to a program). After each inputted 
@@ -22,3 +23,36 @@ dollar sign ($) and formatted to two decimal places. Treat the user's input
 case insensitively. Ignore any input that isn't an item. Assume that every 
 item on the menu will be titlecased.
 """
+
+def main():
+
+    total = 0
+    items = {
+        "Baja Taco": 4.00,
+        "Burrito": 7.50,
+        "Bowl": 8.50,
+        "Nachos": 11.00,
+        "Quesadilla": 8.50,
+        "Super Burrito": 8.50,
+        "Super Quesadilla": 9.50,
+        "Taco": 3.00,
+        "Tortilla Salad": 8.00,
+        }
+
+
+    try:
+        while True:
+            add_order = (input("Item: ")).strip().title()
+
+            if add_order in items.keys():
+                total = total + items[add_order]
+                print(f"Total: {total}")
+
+    
+    except  EOFError:
+        pass
+
+    print('')
+
+if __name__ == "__main__":
+    main()
