52824353355c4f60dda44730a20fd386148e5e4d
Victoria Saravia <victoria.saravia@idatha.com>
2023-10-31 14:51:11 -0300

    reg expressions - response fixed

--- set7_regExpressions/response.py
+++ set7_regExpressions/response.py
@@ -9,7 +9,8 @@ def main():
 def is_valid_email(email):
     pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
     
-    if re.match(pattern, email):
+    # Use the re.match() function to search for a match within the string
+    if re.match(pattern, email) and email.count('@') == 1 and email.count('..') == 0:
         return "Valid"
     else:
         return "Invalid"
