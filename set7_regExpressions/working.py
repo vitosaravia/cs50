b48baab083adbae869bf6279a1f089d453685844
Victoria Saravia <victoria.saravia@idatha.com>
2023-10-31 14:17:43 -0300

    reg expressions - working

--- set7_regExpressions/working.py
+++ set7_regExpressions/working.py
@@ -31,30 +31,34 @@ def convert(time_input):
         time_one, time_two = time_input.split("TO")
     except ValueError:
         sys.exit("Invalid input")
+
     if "AM" in time_one:
         time_format_one = "AM"
     elif "PM" in time_one:
         time_format_one = "PM"
     else:
-        time_format_one = ""
+        sys.exit("Invalid format")
     if "AM" in time_two:
         time_format_two = "AM"
     elif "PM" in time_two:
         time_format_two = "PM"
     else:
-        time_format_two = ""
-    if ":" in time_one and ":" in time_two:
-        time_one = time_one.split(":")
-        time_one = [int(re.search(r'\d+', time_one[0]).group()), int(re.search(r'\d+', time_one[1]).group()), time_format_one]
-        time_two = time_two.split(":")
-        time_two = [int(re.search(r'\d+', time_two[0]).group()), int(re.search(r'\d+ ', time_two[1]).group()), time_format_two]
-
-    elif ":" in time_one or ":" in time_two:
         sys.exit("Invalid format")
+    try:
+        if ":" in time_one and ":" in time_two:
+            time_one = time_one.split(":")
+            time_one = [int(re.search(r'\d+', time_one[0]).group()), int(re.search(r'\d+', time_one[1]).group()), time_format_one]
+            time_two = time_two.split(":")
+            time_two = [int(re.search(r'\d+', time_two[0]).group()), int(re.search(r'\d+ ', time_two[1]).group()), time_format_two]
 
-    else:
-        time_one = [int(re.search(r'\d+', time_one).group()), 0, time_format_one]
-        time_two = [int(re.search(r'\d+', time_two).group()), 0, time_format_two]
+        elif ":" in time_one or ":" in time_two:
+            sys.exit("Invalid format")
+
+        else:
+            time_one = [int(re.search(r'\d+', time_one).group()), 0, time_format_one]
+            time_two = [int(re.search(r'\d+', time_two).group()), 0, time_format_two]
+    except:
+        sys.exit("Invalid input")
 
     if time_format_one == "PM":
         time_one[0] = time_one[0] + 12
