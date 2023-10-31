89da3c5b8fbc6165b2ea8d1bd06f611342d18569
Victoria Saravia <victoria.saravia@idatha.com>
2023-10-31 15:33:41 -0300

    objOriented - seasons

---
+++ set8_objOriented/seasons.py
@@ -0,0 +1,19 @@
+"""implement a program that prompts the user for their date 
+of birth in YYYY-MM-DD format and then sings prints how old 
+they are in minutes, rounded to the nearest integer, using 
+English words instead of numerals, just like the song from 
+Rent, without any and between words. Since a user might not 
+know the time at which they were born, assume, for 
+simplicity, that the user was born at midnight 
+(i.e., 00:00:00) on that date. And assume that the current 
+time is also midnight. In other words, even if the user runs 
+the program at noon, assume that it's actually midnight, on 
+the same date. Use datetime.date.today to get today's date, 
+per docs.python.org/3/library/datetime.html#datetime.date.today."""
+
+
+def main():
+    ...
+
+if __name__ == "__main__":
+    main()
